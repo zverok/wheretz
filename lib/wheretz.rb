7f232a45d910b20ca3808891def47693039f8765
zverok <zverok.offline@gmail.com>
2016-01-28 16:45:12 +0200

    Make Rubocop happier

--- lib/wheretz.rb
+++ lib/wheretz.rb
@@ -1,25 +1,30 @@
 require 'geo_ruby'
 require 'geo_ruby/geojson'
 
+# WhereTZ is ...
 module WhereTZ
-  FILES = Dir[File.expand_path('../../data/*.geojson', __FILE__)].map{|f|
-    zone, *coords = f.sub(/^.+data\//, '').sub('.geojson', '').split('__')
-    zone = zone.gsub('-', '/')
-    minx, maxx, miny, maxy = coords.map(&:to_f)
-    [f, zone, minx..maxx, miny..maxy]
-  }.freeze
+  FILES =
+    Dir[File.expand_path('../../data/*.geojson', __FILE__)].
+    map{|f|
+      name = File.basename(f).sub('.geojson', '')
+      zone, *coords = name.split('__')
+      zone = zone.tr('-', '/')
+      minx, maxx, miny, maxy = coords.map(&:to_f)
+      [f, zone, minx..maxx, miny..maxy]
+    }.freeze
 
   AmbigousTimezone = Class.new(RuntimeError)
 
   module_function
 
   def lookup(lat, lng)
-    candidates = FILES.select{|fname, zone, x, y| x.cover?(lng) && y.cover?(lat)}
+    candidates =
+      FILES.
+      select{|_f, _z, xr, yr| xr.cover?(lng) && yr.cover?(lat)}
+
     case candidates.size
-    when 0
-      nil
-    when 1
-      candidates.first[1]
+    when 0 then nil
+    when 1 then candidates.first[1]
     else
       lookup_geo(lat, lng, candidates)
     end
@@ -29,7 +34,7 @@ module WhereTZ
     begin
       require 'tzinfo'
     rescue LoadError
-      raise LoadError, "Please install tzinfo for using #get"
+      raise LoadError, 'Please install tzinfo for using #get'
     end
 
     name = lookup(lat, lng)
@@ -37,23 +42,23 @@ module WhereTZ
   end
 
   private
-  module_function
+
   PARSER = GeoRuby::GeoJSONParser.new
-  
+
+  module_function
+
   def lookup_geo(lat, lng, candidates)
     point = GeoRuby::SimpleFeatures::Point.from_coordinates([lng, lat])
 
     candidates = candidates.map{|fname, zone, *|
       [zone, PARSER.parse(File.read(fname)).features.first.geometry]
-    }.select{|zone, multipolygon|
+    }.select{|_, multipolygon|
       multipolygon.geometries.any?{|polygon| polygon.contains_point?(point)}
     }
 
     case candidates.size
-    when 0
-      nil
-    when 1
-      candidates.first.first
+    when 0 then nil
+    when 1 then candidates.first.first
     else
       fail(AmbigousTimezone, "Ambigous timezone: #{candidates.map(&:first)}")
     end
